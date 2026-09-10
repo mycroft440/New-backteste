2f707daf22f059e46f93d534ddf47a83b9a92807
mycroft440 <alquimistadodetalhe@gmail.com>
2026-09-10 18:01:31 -0300

    portfolio: expose champion portfolio manager

--- src/b3_backtest/portfolio/__init__.py
+++ src/b3_backtest/portfolio/__init__.py
@@ -1,10 +1,24 @@
+from .b3lab_champion import (
+    CHAMPION_INDICATOR_NAME,
+    CHAMPION_MANAGEMENT_STRATEGY_NAME,
+    ChampionSelection,
+    champion_source_metadata,
+    select_b3lab_champion,
+)
+from .champion_engine import simulate_b3lab_champion_portfolio
 from .engine import ManagedPortfolioResult, simulate_managed_portfolio
 from .top1_momentum import MANAGEMENT_STRATEGY_NAME, RebalanceSelection, select_top1_momentum
 
 __all__ = [
+    "CHAMPION_INDICATOR_NAME",
+    "CHAMPION_MANAGEMENT_STRATEGY_NAME",
+    "ChampionSelection",
     "MANAGEMENT_STRATEGY_NAME",
     "ManagedPortfolioResult",
     "RebalanceSelection",
+    "champion_source_metadata",
+    "select_b3lab_champion",
     "select_top1_momentum",
+    "simulate_b3lab_champion_portfolio",
     "simulate_managed_portfolio",
 ]
