4bda4fd4e94e5b72a5f39ffc660bd5d00a0ee6f3
mycroft440 <alquimistadodetalhe@gmail.com>
2026-09-10 17:59:30 -0300

    indicators: expose B3 Strategy Lab champion signal

---
+++ src/b3_backtest/indicators/__init__.py
@@ -0,0 +1,25 @@
+from .b3lab_champion import (
+    ChampionIndicatorSnapshot,
+    ROC_WINDOWS,
+    SHORT_WINDOW,
+    SOURCE_CODE_COMMIT,
+    SOURCE_CONFIG,
+    SOURCE_RESULT_COMMIT,
+    TREND_WINDOW,
+    VOL_WINDOW,
+    evaluate_champion_indicator,
+    evaluate_universe,
+)
+
+__all__ = [
+    "ChampionIndicatorSnapshot",
+    "ROC_WINDOWS",
+    "SHORT_WINDOW",
+    "SOURCE_CODE_COMMIT",
+    "SOURCE_CONFIG",
+    "SOURCE_RESULT_COMMIT",
+    "TREND_WINDOW",
+    "VOL_WINDOW",
+    "evaluate_champion_indicator",
+    "evaluate_universe",
+]
