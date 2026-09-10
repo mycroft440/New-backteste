91671320dfe8ff265710c56ac108d1e93d9b921f
mycroft440 <alquimistadodetalhe@gmail.com>
2026-09-10 14:38:55 -0300

    strategies: expose signal catalog API

---
+++ src/b3_backtest/strategies/__init__.py
@@ -0,0 +1,19 @@
+from .catalog import (
+    CATALOG,
+    SPECS,
+    StrategySpec,
+    generate_events,
+    get_strategy,
+    list_strategies,
+    run_strategy,
+)
+
+__all__ = [
+    "CATALOG",
+    "SPECS",
+    "StrategySpec",
+    "generate_events",
+    "get_strategy",
+    "list_strategies",
+    "run_strategy",
+]
