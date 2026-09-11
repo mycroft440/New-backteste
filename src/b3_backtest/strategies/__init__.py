98dd8abd7df69ba90c8a2a297700965c5b7148c2
mycroft440 <alquimistadodetalhe@gmail.com>
2026-09-10 21:33:22 -0300

    strategies: expose full and original catalog views

--- src/b3_backtest/strategies/__init__.py
+++ src/b3_backtest/strategies/__init__.py
@@ -1,19 +1,27 @@
 from .catalog import (
     CATALOG,
+    NATIVE_SPECS,
     SPECS,
+    VENDORED_SPECS,
     StrategySpec,
     generate_events,
     get_strategy,
+    list_original_strategies,
     list_strategies,
+    list_strategy_batches,
     run_strategy,
 )
 
 __all__ = [
     "CATALOG",
+    "NATIVE_SPECS",
     "SPECS",
+    "VENDORED_SPECS",
     "StrategySpec",
     "generate_events",
     "get_strategy",
+    "list_original_strategies",
     "list_strategies",
+    "list_strategy_batches",
     "run_strategy",
 ]
